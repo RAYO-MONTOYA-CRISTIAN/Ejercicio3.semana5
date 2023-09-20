d71a09e63688cf1e0fa07e51ade9f9aba9bf66e8
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-09-20 17:55:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerccio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de notas: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            int suma = 0;
+            int promedio = 0;
+            for (int i = 0; i < limite; i++)
+            {
+                Console.WriteLine("Ingrese la nota " + (i + 1));
+                int numero = int.Parse(Console.ReadLine());
+                suma = suma + numero;
+                promedio = suma / limite;
+            }
+            Console.WriteLine("El promedio es: " + promedio);
+
+        }
+    }
+}
